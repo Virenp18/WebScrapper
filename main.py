6138646fbf9b916852270f08df28f855cc78f8ff
Varend Pratap <48083631+Virenp18@users.noreply.github.com>
2024-04-25 23:05:03 -0400

    Added Scorecard Scrapping

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests,json
+from bs4 import BeautifulSoup
+
+url = "https://cricclubs.com/ScarboroughCA/viewScorecard.do?matchId=5253&clubId=1835"
+
+r = requests.get(url)
+htmlContent = r.content
+
+# print(htmlContent)
+
+soup = BeautifulSoup(htmlContent,'html.parser')
+teamone_batting_array = []
+# getting the invaders B batting scorecard
+invaders_batting = soup.find("div", {"id": "ballByBallTeam2"})
+if invaders_batting:
+    tbody_tag = invaders_batting.find("tbody")
+    if tbody_tag:
+        all_batsmanscore = tbody_tag.findAll("tr")
+        for abs in all_batsmanscore:
+            valid_tr = abs.find('img')
+            if valid_tr:
+                player_detail = {}
+                one_bat_all_anc = abs.findAll("a")
+                player_name = one_bat_all_anc[0]
+                player_detail["name"] = "".join(one_bat_all_anc[0].get_text().split())
+                batting_fig = abs.find_all("th")
+                player_detail["runs"] = "".join(batting_fig[2].get_text().split())       
+                player_detail["balls"] = "".join(batting_fig[3].get_text().split())       
+                player_detail["fours"] = "".join(batting_fig[4].get_text().split())      
+                player_detail["six"] = "".join(batting_fig[5].get_text().split())    
+                player_detail["sr"] = "".join(batting_fig[6].get_text().split())   
+                teamone_batting_array.append(player_detail)
+
+json_data = json.dumps(teamone_batting_array)
+print(json_data)
